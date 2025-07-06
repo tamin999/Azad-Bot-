a6eb2b655f79e6e57c5dccbb19cf9b138bdf4582
Bot-azad <mdazad09188@gmail.com>
2025-07-07 03:44:22 +0600

    Update slot.js

--- scripts/cmds/slot.js
+++ scripts/cmds/slot.js
@@ -34,7 +34,7 @@ module.exports = {
       return message.reply(getLang("not_enough_money"));
     }
 
-    const slots = ["💨", "🔳", "🦆", "🔳", "💨", "🦆", "🦆", "🔳", "💨"];
+    const slots = ["💚", "💛", "💙", "💛", "💚", "💙", "💙", "💛", "💚"];
     const slot1 = slots[Math.floor(Math.random() * slots.length)];
     const slot2 = slots[Math.floor(Math.random() * slots.length)];
     const slot3 = slots[Math.floor(Math.random() * slots.length)];
@@ -53,9 +53,9 @@ module.exports = {
 };
 
 function calculateWinnings(slot1, slot2, slot3, betAmount) {
-  if (slot1 === "💨" && slot2 === "💨" && slot3 === "💨") {
+  if (slot1 === "💚" && slot2 === "💚" && slot3 === "💚") {
     return betAmount * 10;
-  } else if (slot1 === "🔳" && slot2 === "🔳" && slot3 === "🔳") {
+  } else if (slot1 === "💛" && slot2 === "💛" && slot3 === "💛") {
     return betAmount * 5;
   } else if (slot1 === slot2 && slot2 === slot3) {
     return betAmount * 3;
@@ -68,12 +68,12 @@ function calculateWinnings(slot1, slot2, slot3, betAmount) {
 
 function getSpinResultMessage(slot1, slot2, slot3, winnings, getLang) {
   if (winnings > 0) {
-    if (slot1 === "🦆" && slot2 === "🦆" && slot3 === "🦆") {
-      return getLang("jackpot_message", winnings, "🦆");
+    if (slot1 === "💙" && slot2 === "💙" && slot3 === "💙") {
+      return getLang("jackpot_message", winnings, "💙");
     } else {
       return getLang("win_message", winnings) + `\[ ${slot1} | ${slot2} | ${slot3} ]`;
     }
   } else {
     return getLang("lose_message", -winnings) + `\[ ${slot1} | ${slot2} | ${slot3} ]`;
   }
-}
+      }
