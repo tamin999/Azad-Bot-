fd67315d90de5f7a2adc62d04a18235e62b94475
Bot-azad <mdazad09188@gmail.com>
2025-07-07 02:27:59 +0600

    Create gf.js

---
+++ scripts/cmds/gf.js
@@ -0,0 +1,22 @@
+module.exports = {
+ config: {
+	 name: "gf",
+	 version: "1.0",
+	 author: "BaYjid",
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "no prefix",
+	 longDescription: "no prefix",
+	 category: "no prefix",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "gf") {
+ return message.reply({
+ body: " 「 BESSAR BUKE",
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/nWD4xk3.mp4")
+ });
+ }
+ }
+}
