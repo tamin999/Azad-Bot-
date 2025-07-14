37b4edac860561939b53d74e3b42c7eee21ac5d9
Bot-azad <mdazad09188@gmail.com>
2025-07-15 02:01:08 +0600

    Update uid.js

--- scripts/cmds/uid.js
+++ scripts/cmds/uid.js
@@ -1,63 +1,62 @@
-const { findUid } = global.utils;
-const regExCheckURL = /^(http|https):\/\/[^ "]+$/;
+const axios = require("axios");
+const fs = require("fs-extra");
+const path = require("path");
 
 module.exports = {
-	config: {
-		name: "uid",
-		version: "1.3",
-		author: "NTKhang",
-		countDown: 5,
-		role: 0,
-		description: {
-			vi: "Xem user id facebook của người dùng",
-			en: "View facebook user id of user"
-		},
-		category: "info",
-		guide: {
-			vi: "   {pn}: dùng để xem id facebook của bạn"
-				+ "\n   {pn} @tag: xem id facebook của những người được tag"
-				+ "\n   {pn} <link profile>: xem id facebook của link profile"
-				+ "\n   Phản hồi tin nhắn của người khác kèm lệnh để xem id facebook của họ",
-			en: "   {pn}: use to view your facebook user id"
-				+ "\n   {pn} @tag: view facebook user id of tagged people"
-				+ "\n   {pn} <profile link>: view facebook user id of profile link"
-				+ "\n   Reply to someone's message with the command to view their facebook user id"
-		}
-	},
+  config: {
+    name: "uid",
+    aliases: ["id"],
+    version: "1.1",
+    author: "Azad Vai",
+    countDown: 3,
+    role: 0,
+    shortDescription: "Get UID and profile pic",
+    longDescription: "Get user UID with profile picture by reply or mention",
+    category: "info",
+    guide: {
+      en: "{pn} [@mention/reply] - Get user UID and profile picture"
+    }
+  },
 
-	langs: {
-		vi: {
-			syntaxError: "Vui lòng tag người muốn xem uid hoặc để trống để xem uid của bản thân"
-		},
-		en: {
-			syntaxError: "Please tag the person you want to view uid or leave it blank to view your own uid"
-		}
-	},
+  onStart: async function ({ event, message, usersData, api }) {
+    let uid;
+    let name;
 
-	onStart: async function ({ message, event, args, getLang }) {
-		if (event.messageReply)
-			return message.reply(event.messageReply.senderID);
-		if (!args[0])
-			return message.reply(event.senderID);
-		if (args[0].match(regExCheckURL)) {
-			let msg = '';
-			for (const link of args) {
-				try {
-					const uid = await findUid(link);
-					msg += `${link} => ${uid}\n`;
-				}
-				catch (e) {
-					msg += `${link} (ERROR) => ${e.message}\n`;
-				}
-			}
-			message.reply(msg);
-			return;
-		}
+    // If message is a reply
+    if (event.type === "message_reply") {
+      uid = event.messageReply.senderID;
+      name = event.messageReply.senderID == event.senderID ? "Your" : (await usersData.getName(uid));
+    }
 
-		let msg = "";
-		const { mentions } = event;
-		for (const id in mentions)
-			msg += `${mentions[id].replace("@", "")}: ${id}\n`;
-		message.reply(msg || getLang("syntaxError"));
-	}
+    // If someone is mentioned
+    else if (event.mentions && Object.keys(event.mentions).length > 0) {
+      uid = Object.keys(event.mentions)[0];
+      name = await usersData.getName(uid);
+    }
+
+    // If nothing, then self UID
+    else {
+      uid = event.senderID;
+      name = "Your";
+    }
+
+    // Get avatar
+    const avatarUrl = `https://graph.facebook.com/${uid}/picture?width=720&height=720`;
+    const imgPath = path.join(__dirname, "cache", `${uid}.jpg`);
+
+    try {
+      const response = await axios.get(avatarUrl, { responseType: "arraybuffer" });
+      fs.writeFileSync(imgPath, Buffer.from(response.data, "binary"));
+
+      await message.reply({
+        body: `${name} UID: ${uid}`,
+        attachment: fs.createReadStream(imgPath)
+      });
+
+      fs.unlinkSync(imgPath); // Delete after send
+    } catch (err) {
+      console.error(err);
+      message.reply(`${name} UID: ${uid}\n(But couldn't load profile picture)`);
+    }
+  }
 };
